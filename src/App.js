149d6db58066dbf4d104fded3e1705e9fc4e06d5
Isham Jassat <isham.jassat@gmail.com>
2020-07-06 13:54:58 +0100

    Implement basic search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, Component } from 'react';
 import './App.css';
+import Async from 'react-async';
+import Chart from "react-apexcharts"
+const axios = require('axios').default;
+
+
+const casesApiUrl = 'https://c19downloads.azureedge.net/downloads/json/coronavirus-cases_latest.json';
+const deathsApiUrl = 'https://c19downloads.azureedge.net/downloads/json/coronavirus-deaths_latest.json';
+let chartData = [];
+let chartAxis = [];
+let chartAvg = [];
+let options = {};
+
+const covidData = ({place}) => 
+  axios.get(casesApiUrl)
+    .then(function(response) {
+      //return filtered data
+      return response.data.utlas.filter(function(la) {
+        return la.areaName === place;
+      });
+    }).catch(function(error) {
+      console.log(error);
+    })
+
+
 
 function App() {
+  const [plc, setPlace] = useState('East Sussex')
+
+  const getPlace = (e) => {
+    const newPlace = e.target.previousElementSibling.value;
+    setPlace(newPlace);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" id="place-input" /><button id="submit-btn" onClick={getPlace}>Get data</button>
+      <Async promiseFn = {covidData} place={plc} watch={plc}>
+        {({ data, err, isLoading }) => {
+          if (isLoading) return "Loading..."
+          if (err) return `Something went wrong: ${err.message}`
+          if (data)
+            if(typeof data[0] === 'undefined') {
+              return 'No data...'
+            } else {
+            chartData = [];
+            chartAxis = [];
+            data.map(item => {
+              chartData.unshift(item.dailyLabConfirmedCases);
+              chartAxis.unshift(item.specimenDate);
+            })
+            options = {
+              options: {
+                chart: {
+                  id: "basic-bar"
+                },
+                xaxis: {
+                  categories: chartAxis
+                },
+                stroke: {
+                  curve: 'smooth'
+                },
+                dataLabels: {
+                  enabled: false
+                }
+              },
+              series: [
+                {
+                  name: data[0].areaName,
+                  data: chartData
+                }
+              ]
+            };
+            return (
+              <div id="main">
+                <h1>Data for {data[0].areaName}</h1>
+                <Chart 
+                  options={options.options}
+                  series={options.series}
+                  type='area'
+                  width='90%'
+                  height='90%'
+                >
+                </Chart>
+              </div>
+            )
+            }
+        }}
+      </Async>
     </div>
   );
 }
